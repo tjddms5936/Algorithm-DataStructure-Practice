4ab18ce0ab9e3abe90be1fb5601962caabb459a1
ParkSungEun <tjddms5936@naver.com>
2023-06-14 19:25:40 +0900

    삽입 정렬

---
+++ insertion_sort/insertion_sort/insertion_sort.cpp
@@ -0,0 +1,64 @@
+﻿// insertion_sort.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+void insertSortLogic(int* pData, int nLength, int nInsertData);
+void insertSort(int* pData, int nLength);
+void printData(int* pData, int nLength);
+
+int main()
+{
+    int arData[9]{ 6,5,2,4,3,1,7,9,8 };
+    insertSort(arData, 9);
+
+    printData(arData, 9);
+}
+
+void insertSortLogic(int* pData, int nLength, int nInsertData)
+{
+    int nCompareData{};
+    int nCompareIndex{};
+    int nBackupIndex{};
+
+    nCompareIndex = nLength - 2;
+    nCompareData = pData[nCompareIndex];
+    nBackupIndex = nCompareIndex + 1;
+
+    while (nCompareIndex >= 0 && nCompareData > nInsertData)
+    {
+        pData[nBackupIndex] = nCompareData;
+
+        nBackupIndex = nCompareIndex;
+        nCompareIndex--;
+        nCompareData = pData[nCompareIndex];
+    }
+
+    pData[nBackupIndex] = nInsertData;
+
+}
+
+void insertSort(int* pData, int nLength)
+{
+    int nCompareLength{};
+    int nInsertData{};
+
+    nCompareLength = 2;
+
+    while (nCompareLength <= nLength)
+    {
+        nInsertData = pData[nCompareLength - 1];
+        insertSortLogic(pData, nCompareLength, nInsertData);
+        nCompareLength++;
+    }
+
+}
+
+void printData(int* pData, int nLength)
+{
+    for (int i = 0; i < nLength; i++)
+    {
+        printf("%d ", pData[i]);
+    }
+    printf("\n");
+}
