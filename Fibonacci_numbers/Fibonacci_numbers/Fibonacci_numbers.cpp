fd5dc74f28d89e268d02ac3e3259a9c4035443c5
ParkSungEun <tjddms5936@naver.com>
2023-06-14 19:34:56 +0900

    피보나치 수열 : 재귀적 구성

---
+++ Fibonacci_numbers/Fibonacci_numbers/Fibonacci_numbers.cpp
@@ -0,0 +1,33 @@
+﻿// Fibonacci_numbers.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+void fibonacci(int nIndex, int* pResult);
+
+int main()
+{
+    int nData{};
+
+
+    fibonacci(8, &nData);
+    printf("%d\n", nData);
+
+}
+
+void fibonacci(int nIndex, int* pResult)
+{
+    if (nIndex <= 1)
+    {
+        *pResult = 1;
+        return;
+    }
+
+    int nResult1{};
+    int nResult2{};
+
+    fibonacci(nIndex - 2, &nResult1);
+    fibonacci(nIndex - 1, &nResult2);
+
+    *pResult = nResult1 + nResult2;
+}
